36f2504091f50be78371c567be4e5344218aa0f2
Alberto Lacerda <alberto.p.lacerda@hotmail.com>
2021-03-12 17:52:55 -0300

    Alterado arquivo FormacaoServices

--- UniLacerda/Services/CursoServices.cs
+++ UniLacerda/Services/CursoServices.cs
@@ -1,8 +1,25 @@
+using System.Diagnostics;
+
+using System;
+using System.Collections.Generic;
+using MongoDB.Driver;
+using Microsoft.Extensions.Configuration;
+using Models;
 
 namespace Services
 {
     public class CursoServices
+
     {
+        private readonly IMongoCollection<Curso> _cursos;
+
         //senha do banco Pg7i0vWRAjhbm04Y User api nome banco UniLacerdaDB
+        public CursoServices(IConfiguration configuration)
+        {
+            var cli=new MongoClient(configuration.GetConnectionString("Conexao"));
+            var db=cli.GetDatabase("UniLacerdaDB");
+            _cursos=db.GetCollection<Curso>("Curso");
+            
+        }
     }
 }
--- UniLacerda/Services/FormacaoServices.cs
+++ UniLacerda/Services/FormacaoServices.cs
@@ -1,7 +1,39 @@
+
+using System.Reflection.Metadata;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+using System.Diagnostics;
+
+using System;
+using System.Collections.Generic;
+using MongoDB.Driver;
+using Microsoft.Extensions.Configuration;
+using Models;
 namespace Services
 {
     public class FormacaoServices
     {
-        
+        private readonly IMongoCollection<Formacao> _formacao;
+
+        public FormacaoServices(IConfiguration config)
+        {
+            var cli= new MongoClient(config.GetConnectionString("Conexao"));
+            var db= cli.GetDatabase("UniLacerdaDB");
+            _formacao = db.GetCollection<Formacao>("Formacao");            
+        }
+
+        public List<Formacao> Index() => _formacao.Find(Formacao => true).ToList();
+        public Formacao GetFormacao(string id) => _formacao.Find(formacao => formacao.Id == id).FirstOrDefault();
+        public Formacao SaveFormacao(Formacao formacao){
+            _formacao.InsertOne(formacao);
+            return formacao;
+        }
+
+        public Formacao DeleteFormacao(string id)
+        {
+              var formacao= _formacao.Find(formacao=> formacao.Id == id).FirstOrDefault();
+              _formacao.DeleteOne(formacao=> formacao.Id ==id);
+              return formacao;
+        }
     }
 }
