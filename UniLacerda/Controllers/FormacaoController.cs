d7302a05f53f377a6d6448abba05eced92e90d3a
Alberto Lacerda <alberto.p.lacerda@hotmail.com>
2021-03-12 18:27:32 -0300

    Alterado Controller

--- UniLacerda/Controllers/FormacaoController.cs
+++ UniLacerda/Controllers/FormacaoController.cs
@@ -1,7 +1,21 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+using Services;
+
 namespace Controllers
 {
+    [ApiController]
+    [Route("[controller]")]
     public class FormacaoController : ControllerBase
     {
+        private readlonly FormacaoServices _services;
+        public FormacaoController(FormacaoServices services)
+        {
+            _services= services;
+        }
+
+        
         
     }
 }
