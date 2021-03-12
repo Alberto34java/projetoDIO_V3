35ecba7109bd2b18b1b98e5bc99efa47361d95d8
Alberto Lacerda <alberto.p.lacerda@hotmail.com>
2021-03-11 21:09:36 -0300

    Alterado

--- UniLacerda/Models/Curso.cs
+++ UniLacerda/Models/Curso.cs
@@ -1,10 +1,20 @@
+using MongoDB.Bson.Serialization.Attributes;
+
 namespace Models
 {
     public class Curso
     {
+         [BsonRequired]
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
        public string Id { get; set; }   
+       [BsonElement]
        public string  Nome { get; set; }
+        [BsonElement]
+        [BsonRequired]
        public string  Descricao { get; set; }
+        [BsonElement]
+         [BsonRequired]
        public string Duracao { get; set; }
        public Formacao Formacao { get; set; }
        public string FormacaoId { get; set; }
--- UniLacerda/Models/Formacao.cs
+++ UniLacerda/Models/Formacao.cs
@@ -4,9 +4,11 @@ namespace Models
 {
     public class Formacao
     {
+         [BsonRequired]
         [BsonId]
         [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
         public string Id { get; set; }
+         [BsonRequired]
         [BsonElement]
         public string Nome { get; set; }
     }
