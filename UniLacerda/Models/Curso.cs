cd32bc897b93c2e2c2f25f755bdc851eddf081e0
Alberto Lacerda <alberto.p.lacerda@hotmail.com>
2021-03-11 21:01:18 -0300

    Criado Segundo Model

---
+++ UniLacerda/Models/Curso.cs
@@ -0,0 +1,11 @@
+namespace Models
+{
+    public class Curso
+    {
+       public string Id { get; set; }   
+       public string  Nome { get; set; }
+       public string  Descricao { get; set; }
+       public string Duracao { get; set; }
+       
+    }
+}
